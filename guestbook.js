b06846b7aad7aed9aca3f29b8c672d59c5a155ba
Tiberiu-Catalin Munteanu <53290917+Tibs12@users.noreply.github.com>
2022-08-25 15:59:13 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://04734a69.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
